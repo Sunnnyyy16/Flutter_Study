2610dc49252258575b6cd0251ade65488745556d
Sunnnyyy16 <yjs98101@gmail.com>
2023-08-10 11:21:54 +0900

    [feat]create main page without custom widget

---
+++ second_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 82, 33, 166),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 200,
+              ),
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  height: 5,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                child: const Text('Start Quiz'),
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
