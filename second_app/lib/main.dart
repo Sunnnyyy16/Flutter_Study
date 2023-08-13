8d514f9f56b1e771b14beb8ab56359fc5201aaae
Sunnnyyy16 <yjs98101@gmail.com>
2023-08-13 10:06:34 +0900

    create 2 StatefulWidget

--- second_app/lib/main.dart
+++ second_app/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:second_app/start_screen.dart';
+import 'package:second_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 66, 20, 146),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ second_app/lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:second_app/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState(){
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz>{
+  @override
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 66, 20, 146),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
