12728057d06d17d03ef55239a9c098122c8dd685
Sunnnyyy16 <yjs98101@gmail.com>
2023-11-02 12:15:18 +0900

    add maps with for loop in results screen

--- second_app/lib/results_screen.dart
+++ second_app/lib/results_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({
-    super.key, 
-    required this.chosenAnswers
-    });
+  const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      //loop body
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer':questions[i].answers[0], // 1st answer is correct one
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +32,17 @@ class ResultsScreen extends StatelessWidget {
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
+          children: [
             const Text('You answered X out of Y questions correctly!'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             const Text('List of answers and questions...'),
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             TextButton(
-              onPressed: () {}, 
+              onPressed: () {},
               child: const Text('Restart Quiz!'),
             )
           ],
